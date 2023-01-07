a5a12e6cda630feb9bf4c8cfadec39e271d1c2a7
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2023-01-07 09:39:50 -0800

    Day 37 - Project 7, part 2 - Initial app.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,23 +7,9 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let firstName: String
-    let lastName: String
-}
-
 struct ContentView: View {
-    
-    @State private var user = User(firstName: "Matt", lastName: "Dolan")
-    
     var body: some View {
-        Button("Save User") {
-            let encoder = JSONEncoder()
-            
-            if let data = try? encoder.encode(user) {
-                UserDefaults.standard.set(data, forKey: "UserData")
-            }
-        }
+        Text("Hello, world!")
     }
 }
 
