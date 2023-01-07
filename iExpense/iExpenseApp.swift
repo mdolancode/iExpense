7ce4d2459b76fcf37878c4442d63dad1e65d72bd
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2023-01-06 16:27:40 -0800

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Matthew Dolan on 2023-01-06.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
