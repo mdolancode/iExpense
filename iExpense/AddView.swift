335984cb6128f82b3c7b32d58794f2df6a235c7c
Matthew Dolan <matthew.dolan@ballysinteractive.com>
2023-01-11 16:45:52 -0800

    Day 37 - Project 7, part 2 - Sharing an observed object with a new view.

---
+++ iExpense/AddView.swift
@@ -0,0 +1,44 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Matthew Dolan on 2023-01-11.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @ObservedObject var expenses: Expenses
+
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
