4ce3e78ab8e19e5f39d89953de6064251237e12c
Matthew Dolan <mdolancode@gmail.com>
2023-03-03 14:44:25 -0800

    Create expense section view.

---
+++ iExpense/ExpenseSection.swift
@@ -0,0 +1,39 @@
+//
+//  ExpenseSection.swift
+//  iExpense
+//
+//  Created by Matthew Dolan on 2023-03-02.
+//
+
+import SwiftUI
+
+struct ExpenseSection: View {
+    let title: String
+    let expenses: [ExpenseItem]
+    let deleteItems: (IndexSet) -> Void
+    
+    var body: some View {
+        Section(title) {
+            ForEach(expenses) { item in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(item.name)
+                            .font(.headline)
+                        Text(item.type)
+                    }
+                    
+                    Spacer()
+                    Text(item.amount, format: .localCurrency)
+                        .style(for: item)
+                }
+            }
+            .onDelete(perform: deleteItems)
+        }
+    }
+}
+
+struct ExpenseSection_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpenseSection(title: "Example", expenses: []) { _ in }
+    }
+}
